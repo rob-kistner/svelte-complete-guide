dd627f65bbd1ec6ec070551aed221fa2f310d76e
rob-kistner <robkistner@mac.com>
2019-05-15 11:52:54 -0500

    8

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  let name = 'Max';
+  let age = 30;
+
+  function incrementAge() {
+    age += 1
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<p>My age is {age}.</p>
+<button on:click="{incrementAge}">Change Age</button>
